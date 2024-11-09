dafb2be5cb7bbd64045b00038a24417bc9fd5c4c
Anon Dev <michaelerastus9@gmail.com>
2024-11-09 21:37:05 +0100

    Updated

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -185,38 +185,22 @@
             <div class="row">
                 <div class="col-lg-12">
                     <!-- TradingView Widget BEGIN -->
-                    <div class="tradingview-widget-container">
-                        <div class="tradingview-widget-container__widget"></div>
+                    <div class="tradingview-widget-container" style="height:100%;width:100%">
+                        <div class="tradingview-widget-container__widget" style="height:calc(100% - 32px);width:100%"></div>
                         <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank"><span class="blue-text">Track all markets on TradingView</span></a></div>
-                        <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-forex-cross-rates.js" async>
+                        <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js" async>
                             {
                                 "width": "100%",
-                                "height": 600,
-                                "currencies": [
-                                "EUR",
-                                "USD",
-                                "JPY",
-                                "GBP",
-                                "CHF",
-                                "AUD",
-                                "CAD",
-                                "NZD",
-                                "CNY",
-                                "TRY",
-                                "SEK",
-                                "NOK",
-                                "DKK",
-                                "ZAR",
-                                "HKD",
-                                "SGD",
-                                "THB",
-                                "MXN",
-                                "IDR"
-                            ],
-                                "isTransparent": false,
-                                "colorTheme": "dark",
+                                "height": "610",
+                                "symbol": "BINANCE:BTCUSDT",
+                                "interval": "60",
+                                "timezone": "Etc/UTC",
+                                "theme": "light",
+                                "style": "1",
                                 "locale": "en",
-                                "backgroundColor": "#000000"
+                                "allow_symbol_change": true,
+                                "calendar": false,
+                                "support_host": "https://www.tradingview.com"
                             }
                         </script>
                     </div>

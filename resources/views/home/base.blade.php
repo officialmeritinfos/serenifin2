820f223553e6ae5ab054487423da659e72ffe650
Anon Dev <michaelerastus9@gmail.com>
2025-01-17 08:15:20 +0100

    Completed

--- resources/views/home/base.blade.php
+++ resources/views/home/base.blade.php
@@ -328,7 +328,18 @@
 
         </div>
     </footer>
-
+    <!-- Smartsupp Live Chat script -->
+    <script type="text/javascript">
+        var _smartsupp = _smartsupp || {};
+        _smartsupp.key = 'f0a443870f707f9b563eb77aa21fa21d38b42a6b';
+        window.smartsupp||(function(d) {
+            var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+            s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+            c.type='text/javascript';c.charset='utf-8';c.async=true;
+            c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+        })(document);
+    </script>
+    <noscript> Powered by <a href=“https://www.smartsupp.com” target=“_blank”>Smartsupp</a></noscript>
 </div>
 <!--End pagewrapper-->
 
@@ -352,8 +363,5 @@
 <script src="{{asset('home/js/odometer.js')}}"></script>
 <script src="{{asset('home/js/jquery-ui.js')}}"></script>
 <script src="{{asset('home/js/script.js')}}"></script>
-
-
-
 </body>
 </html>
